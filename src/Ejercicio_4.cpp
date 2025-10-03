f7dd0c88ed056009d3420d0281c355b9702237ac
Fernando5899 <f.reynosa.v023@edu.utc.mx>
2025-10-03 17:08:31 -0600

    Se eliminan los requerimietos de los ejercicios

--- src/Ejercicio_4.cpp
+++ src/Ejercicio_4.cpp
@@ -1,16 +1,3 @@
-// Ejercicio 4: Encontrar el elemento mayor
-/* En una competencia internacional participan N ciclistas a los que
- * se les registra la distancia en metros. Se solicita escribir un programa
- * en C++ y Java que llene un vector con números aleatorios entre 0 y 2500.
- * Encontrar la distancia más larga recorrida por los ciclistas.
- *
- * Entrada de Ejemplo: N = 8, Vector [1510, 24, 878, 1351, 2024, 2145, 154, 542]
- *
- * Salida Esperada: Distancia más larga 2145.
- *
- * Pista: Usar una variable auxiliar para el mayor.
- */
-
 #include <iostream>
 
 using namespace std;
--- src/Ejercicio_6.cpp
+++ src/Ejercicio_6.cpp
@@ -1,25 +1,3 @@
-// Ejercicio 6: Encontrar un elemento menor y si posición
-
-/*
- * Objetivo: practicar la lógica de comparación de elementos sin la necesidad de usar
- * funciones de ordenamiento, para hallar el valor mínimo junto con su posición.
- *
- * Enunciado:en los últimos días se han registrado temblores en M ciudades de México y
- * N ciudades de Colombia. Se solicita escribir un programa en C++ y Java que llene los
- * vectores con números aleatorios entre 0.1 y 9.0.
- * Encontrar las magnitudes más bajas de temblores en ambas ciudades, indicando las
- * posiciones en las que se encuentran en cada arreglo.
- *
- * Entrada de Ejemplo: M = 10, Mexico[4.3, 8.2, 4.8, 2.0, 8.4, 5.8, 3.7, 2.6, 2.5, 7.2]
- *                     N = 12, Colombia[3.5, 4.8, 4.7, 0.9, 1.8, 4.3, 0.8, 4.0, 2.3, 3.5, 4.5, 5.3
- *
- * Salida Esperada: Más baja de México: 2.0 en posición 3.
- *                  Más baja de Colombia: 0.8 en posición 6.
- *
- * Pista: Usar dos variables para las magnitudes más bajas y dos variables
- * para las posiciones de ambos vectores.
- */
-
 #include <iostream>
 #include <ctime>
 using namespace std;
@@ -69,8 +47,8 @@ int main() {
         }
     }
 
-    cout << "\n\nMas baja de mexico " << magnitudMasBajaMexico << " en posicion: " << posicionMexico << ". Del Arreglo.";
-    cout << "\nMas baja en Colombia " << magnitudMasBajaColombia << " en posicion: " << posicionColombia << ". Del Arreglo."<< endl;
+    cout << "\n\nMas baja de mexico " << magnitudMasBajaMexico << " en posicion: " << posicionMexico << ".";
+    cout << "\nMas baja en Colombia " << magnitudMasBajaColombia << " en posicion: " << posicionColombia << "."<< endl;
 
     return 0;
 }
