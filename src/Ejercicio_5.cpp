c0ecaf6dd96e6c7b96d44cccd877bc0c3181507e
Fernando5899 <f.reynosa.v023@edu.utc.mx>
2025-10-03 16:25:50 -0600

    Se acaba el ejercicio 5

--- src/Ejercicio_5.cpp
+++ src/Ejercicio_5.cpp
@@ -14,7 +14,34 @@ using namespace std;
 
 int main() {
 
-    cout << "Ejercicio 5" << endl;
+    // Variable para solicitar el tamaño de nuestro Vector
+    int tamañoVector;
+
+    // Solicitamos un número entero para el tamaño de nuestros vectores
+    // Si pongo ñ en la consola sale un simbolo raro
+    cout << "Introduce el tamano para los vectores: ";
+    cin >> tamañoVector;
+
+    // Le asignamos el tamaño a nuestros vectores
+    int vector_1[tamañoVector];
+    int vector_2[tamañoVector];
+
+    // Ciclo for para rellenar los vectores
+    for (int i = 0; i < tamañoVector; i++) {
+       vector_1[i] = 5 * (i + 5);
+        vector_2[i] = 100 - i * 4;
+    }
+
+    // Ciclo for para imprimir los valores del vector.
+    cout << "Vector 1: ";
+    for (int i = 0; i < tamañoVector; i++) {
+        cout << vector_1[i] << " ";
+    }
+
+    cout << "\nVector 2: ";
+    for (int i = 0; i < tamañoVector; i++) {
+        cout << vector_2[i] << " ";
+    }
 
     return 0;
 }
