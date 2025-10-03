5a7b56e3411326a288cb2401a2f1e53e97b7cfff
Fernando5899 <f.reynosa.v023@edu.utc.mx>
2025-10-03 15:42:48 -0600

    Se ponen los requerimientos dentro de cada Ejercicio

--- src/Ejercicio_5.cpp
+++ src/Ejercicio_5.cpp
@@ -1,3 +1,10 @@
-//
-// Created by FernandoJose on 03/10/2025.
-//
+// Ejercicio 5: Sucesiones de Vectores
+
+/*
+ * Se solicita escribir un programa en c++ y Java que llene dos vectores de N
+ * posiciones con las siguientes sucesiones. Vector 1:
+ * Entrada de Ejemplo: N = 7, Vector1[5, 10, 20, 25, 30, 35]
+ *                            Vector2[100, 85, 70, 55, 40, 25, 10]
+ * Salida esperada: Vector1: 5 10 15 20 25 30 35 y Vector2: 100 85 70 55 40 20 10
+ * Pista: Usa variables de fórmula creativa o fórmulas matemáticas.
+ */
